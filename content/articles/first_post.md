c26e7b45554060d8115def4f34884b137f6fafe6
Sean Azlin <sazlin@gmail.com>
2014-07-05 20:34:52 -0700

    Adding blog project files to repo

---
+++ content/articles/first_post.md
@@ -0,0 +1,9 @@
+Title: My First Blog Post
+Date: 2014-7-05 17:20
+Category: MyCategory
+Tags: Tag1, Tag2
+Slug: first-post
+Author: Your Name
+Summary: This is a my first blog post here.
+
+This is a bunch of awesome content that I've written for my post!
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Sean Azlin'
+SITENAME = u"Sean Azlin's Blog"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'US/Pacific'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (('rheTOracle', 'http://ec2-54-213-173-105.us-west-2.compute.amazonaws.com/'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'http://twitter.com/SeanAzlin2'),
+          ('LinkedIn', 'http://linkedin.com/in/seanazlin'),
+          ('GitHub', 'http://github.com/sazlin'),)
+
+STATIC_PATHS = ['images', ]
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
